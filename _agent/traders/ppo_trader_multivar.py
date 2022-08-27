c58d10d7462e051dc4ca1f12d1bbafbf0d71b0dd
Daniel <dcmay@ualberta.ca>
2022-08-27 12:09:26 -0700

    POO Cleaned
    Remaining changes:
    Add gradient norm
    Add entropy schedule

--- _agent/traders/ppo_trader_multivar.py
+++ _agent/traders/ppo_trader_multivar.py
@@ -116,7 +116,7 @@ class Trader:
         actor_dict, critic_dict = build_actor_critic_models(num_inputs=len(kwargs['observations']),
                                                                                          hidden_actor=kwargs['actor_hidden'],
                                                                                          actor_type=self.actor_type,
-                                                                                         hidden_critic=kwargs['critic_hidden'],
+                                                                                         hidden_critic=kwargs['actor_hidden'],
                                                                                          critic_type=self.critic_type,
                                                                                          num_actions=len(self.actions))
 
@@ -276,7 +276,8 @@ class Trader:
             actor_grads = tape_warmup.gradient(losses_warmup, actor_vars)
             self.ppo_actor.optimizer.apply_gradients(zip(actor_grads, actor_vars))
         else:
-            print('stopping warmup ', max_train_steps - self.train_step, 'steps early')
+            # print('stopping warmup ', max_train_steps - self.train_step, 'steps early')
+            pass
 
         return stop_actor_training
 
@@ -628,7 +629,7 @@ class Trader:
     async def end_of_generation_tasks(self):
         # self.episode_reward_history.append(self.episode_reward)
         episode_G = sum(self.rewards_history)
-        print(self.__participant['id'], 'episode reward:', episode_G)
+        # print(self.__participant['id'], 'episode reward:', episode_G)
 
         data_for_tb = [{'name':'Return', 'data':episode_G, 'type':'scalar', 'step':self.gen},
                        {'name': 'Episode Rewards', 'data': self.rewards_history, 'type': 'histogram', 'step':self.gen},
